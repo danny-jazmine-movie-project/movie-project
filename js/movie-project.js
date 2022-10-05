c19ab854a1f4a924c8675b1dbaa1ddb345d455d6
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-05 12:12:54 -0500

    solved adding movie

--- js/movie-project.js
+++ js/movie-project.js
@@ -1,44 +1,33 @@
-const movieURL = "https://amused-typical-skunk.glitch.me/movies";
-
-/** The C in CRUD: CREATE */
-let idMovie = document.getElementById("addMovie").value
-let ratingMovie = document.getElementById("movieRating").value
-const movieToPost = {
-    title: `${idMovie}`,
-    rating: `${ratingMovie}`
-}
-console.log(idMovie)
-const postMovie = {
-    method: 'POST', // Create a new post
-    headers: {
-        'Content-Type' : 'application/json'
-    },
-    body: JSON.stringify(movieToPost)
-}
-
-function getMovies() {
-    fetch(movieURL).then(resp => resp.json()).then(data=>console.log(data));
-}
-
-// fetch(movieURL, postMovie).then(getMovies); // This will create a new book in the array
-
-getMovies();
 // THIS IS JQUERY
 $(function () {
-
-$("#movieAndRating").on('click', function () {
-    this.append("form")
-});
-});
-
-/** THE D IN CRUD -- Delete */
-
-const deleteOptions = {
-    method: 'DELETE', // Delete a post
-    headers: {
-        'Content-Type' : 'application/json'
+$("#movieAndRating").on('click', function (e) {
+    e.preventDefault();
+    /** The C in CRUD: CREATE */
+    let idMovie = $("#addMovie").val();
+    let ratingMovie = $("#movieRating").val();
+    let movieToPost = {
+        title: `${idMovie}`,
+        rating: `${ratingMovie}`
     }
-}
-// This will modify
-// fetch(movieURL + "/7", deleteOptions).then(getMovies);
+    const movieURL = "https://amused-typical-skunk.glitch.me/movies";
+
+    const postMovie = {
+        method: 'POST', // Create a new post
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(movieToPost)
+    }
+
+    function getMovies() {
+        fetch(movieURL, postMovie).then(resp => resp.json()).then(data=>console.log(data));
+    }
+    getMovies();
+
+    });
+});
+// fetch(movieURL, postMovie).then(getMovies); // This will create a new movie when userInput clicks enter
+
+
+
 
