05bebd08a457c1a21aad9546ca4da74fe0d13537
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-05 14:50:21 -0500

    editing cards

--- js/movie-project.js
+++ js/movie-project.js
@@ -6,52 +6,63 @@ $(function () {
 
     // this console.log our movie data
     function getAllMovieInfo(){
-        fetch(movieURL).then(resp=>resp.json()).then(data => {
+      return fetch(movieURL).then(resp=>resp.json()).then(data => {
             console.log(data)
             movieData = data;
+            return data;
         });
     }
-    getAllMovieInfo();
-    // This function adds new movie
-$("#movieAndRating").on('click', function (e) {
-    e.preventDefault();
-    /** The C in CRUD: CREATE */
-    let idMovie = $("#addMovie").val();
-    let ratingMovie = $("#movieRating").val();
-    let movieToPost = {
-        title: `${idMovie}`,
-        rating: `${ratingMovie}`
-    }
-    const postMovieOptions = {
-        method: 'POST', // Create a new post
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body: JSON.stringify(movieToPost)
-    }
-    function postMovie() {
-        fetch(movieURL, postMovieOptions).then(resp => resp.json()).then(data=>console.log(data));
-    }
-    postMovie();
-    }); // The of the end of on click function
-    /** THE D IN CRUD -- Delete */
+    getAllMovieInfo().then(data=>console.log(data));
+
+    //==================== ADD NEW MOVIE ============================================================
+        // This function adds new movie
+    $("#movieAndRating").on('click', function (e) {
+        e.preventDefault();
+        /** The C in CRUD: CREATE */
+        let idMovie = $("#addMovie").val();
+        let ratingMovie = $("#movieRating").val();
+        let movieToPost = {
+            title: `${idMovie}`,
+            rating: `${ratingMovie}`
+        }
+        const postMovieOptions = {
+            method: 'POST', // Create a new post
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(movieToPost)
+        }
+        function postMovie() {
+            fetch(movieURL, postMovieOptions).then(resp => resp.json()).then(data=>console.log(data));
+        }
+        postMovie();
+        }); // The of the end of on click function
+
+//======================= DELETE ====================================================================
     const deleteOptions = {
         method: 'DELETE', // Delete a post
         headers: {
             'Content-Type' : 'application/json'
         }
     }
-    // fetch(movieURL + "/6", deleteOptions);`
-    movieData.forEach((element, i) => {
-        $(".movieContainer").append(`
+
+//=====================OUTPUT MOVIES ================================================================
+    getAllMovieInfo().then(data => {
+        data.forEach(movieData=> {
+            $("#movieContainer").append(`
             <div class="card">
-                <p>Test Again</p>
-                <p>Title: ${movieData[i].title}</p>
-                <p>Year: ${movieData[i].year}</p>
-                <img width="100%" height="70%" src=${movieData[i].poster}>
+                <p>Title: ${movieData.title}</p>
+                <p>Rating: ${movieData.rating}</p>
+                <p>Year: ${movieData.year}</p>
+                <div class="poster-wrapper">
+                    <img width="100%" height="100%" src=${movieData.poster}/>
+                </div>  
             </div>
             `)
+            console.log(movieData);
+        })
     });
+    // fetch(movieURL + "/6", deleteOptions);`
 });
 
 
